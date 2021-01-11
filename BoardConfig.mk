7684d6578a9d7f0b8bbe02c103ced716c4a99597
retiredtab <68165467+retiredtab@users.noreply.github.com>
2021-01-10 19:19:45 -0700

    Chagallwifi device tree initial upload

---
+++ BoardConfig.mk
@@ -0,0 +1,72 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/chagallwifi
+
+# Platform
+BOARD_VENDOR := samsung
+TARGET_SOC := exynos5420
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+
+# Bootloader
+TARGET_OTA_ASSERT_DEVICE := chagallwifi
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/chagallwifi/releasetools
+TARGET_BOARD_INFO_FILE ?= device/samsung/chagallwifi/board-info.txt
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineageos_chagallwifi_defconfig
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Lineage Hardware
+BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/lineagehw
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2506096640
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12629049344
+BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+# Recovery
+LZMA_RAMDISK_TARGETS := recovery
+
+# Shim
+TARGET_LD_SHIM_LIBS += \
+    /system/bin/gpsd|libshim_gpsd.so
+
+# SELinux
+#BOARD_SEPOLICY_DIRS += device/samsung/exynos5420-common/sepolicy
+#BOARD_SEPOLICY_DIRS += device/samsung/chagallwifi/sepolicy
+
+# Fixes screen flicker
+TARGET_FORCE_SCREENSHOT_CPU_PATH := true
+
+# LineageOS Hardware
+BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/lineagehw
+
+# custom additions to updater-script
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/chagallwifi/releasetools/ota_from_target_files
+
+# Inherit from exynos5420-common
+include device/samsung/exynos5420-common/BoardConfigCommon.mk
---
+++ device.mk
@@ -0,0 +1,101 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/chagallwifi
+
+PRODUCT_CHARACTERISTICS := tablet
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal large xlarge
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.primary.universal5420
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1600
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    fingerprintd \
+    fingerprint.universal5420 \
+    validityService
+
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml
+
+# Ril needed by GPS
+PRODUCT_PACKAGES += \
+    libxml2
+
+# Shim
+PRODUCT_PACKAGES += \
+    libshim_gpsd
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
+
+PRODUCT_PACKAGES += \
+    fstab.universal5420 \
+    init.target.rc \
+    ueventd.universal5420.rc
+
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=288m \
+    dalvik.vm.heapsize=768m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+    
+# https://source.android.com/devices/graphics/renderer
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.zygote.disable_gl_preload=true    
+
+# HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
+
+# call the proprietary setup
+$(call inherit-product, vendor/samsung/chagallwifi/chagallwifi-vendor.mk)
+
+# Import the common tree changes
+include device/samsung/exynos5420-common/exynos5420.mk
